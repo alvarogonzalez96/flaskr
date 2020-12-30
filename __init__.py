14c4516e073864c116835572a14f113e1065d4b1
alvarogonzalez96 <43268965+alvarogonzalez96@users.noreply.github.com>
2020-12-30 17:18:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,6 @@ UPLOAD_FOLDER = os.path.abspath("./flaskr/static/images/")
 
 ALLOWED_EXTENSIONS = set(["png", "jpg", "jpge", "jpeg"])
 
-def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
@@ -14,13 +13,6 @@ def create_app(test_config=None):
         SECRET_KEY=b'\xa1\xbb\xaa\xbe\x15\xa2\x97\x8ccf\xdePsO"\xdc',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
