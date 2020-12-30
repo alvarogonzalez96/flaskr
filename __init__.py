a1ef39fcc2be92cbf9945ab6d22a55bc27f0b1ff
alvarogonzalez96 <43268965+alvarogonzalez96@users.noreply.github.com>
2020-12-30 19:59:54 +0100

    1

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = os.path.abspath("./flaskr/static/images/")
 
 ALLOWED_EXTENSIONS = set(["png", "jpg", "jpge", "jpeg"])
 
-app = create_app(self, test_config=None):
+def create_app(self, test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
@@ -31,6 +31,7 @@ app = create_app(self, test_config=None):
     import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+    
+    return app
 
-
-app.run()
+    
