a6ea54e8e1eee7b0bbc3509082e72115dc92d1ce
alvarogonzalez96 <43268965+alvarogonzalez96@users.noreply.github.com>
2020-12-31 13:25:55 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,22 @@ from __init__ import create_app
 
 from flask import Flask
 
-app = create_app()
+app = create_app(self)
+
+# Load default config and override config from an environment variable
+
+app.config.update(dict(
+
+    DATABASE=os.path.join(app.root_path, 'flaskr.sqlite'),
+
+    DEBUG=True,
+
+    SECRET_KEY='development key',
+
+    USERNAME='admin',
+
+    PASSWORD='default'
+
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
